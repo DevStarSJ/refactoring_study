bb12f0cf58fd1a1cd031516efccc0b01aef13c30
Yun Seokjoon <seokjoon.yun@gmail.com>
2020-04-24 13:55:08 +0900

    for문 2개로 분리

--- src/ch.01.js
+++ src/ch.01.js
@@ -49,15 +49,17 @@ function usd(aNumber) {
 
 function statement(invoice) {
   let totalAmount = 0;
-  let volumnCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumnCredits += volumnCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  let volumnCredits = 0;
+  for (let perf of invoice.performances) {
+    volumnCredits += volumnCreditsFor(perf);
   }
 
   result += `총액: ${usd(totalAmount)}\n`;
