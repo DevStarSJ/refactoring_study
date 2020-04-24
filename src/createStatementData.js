0d207fb4fe50317c4367187b4073fe4cbf8e07cc
Yun Seokjoon <seokjoon.yun@gmail.com>
2020-04-24 15:09:21 +0900

    volumeCreditsFor를 PerformanceCalculator 내부로 이동

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -28,6 +28,14 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = Math.max(this.performance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 function createStatementData(invoice) {
@@ -44,21 +52,13 @@ function createStatementData(invoice) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
 
     return result;
 
     function playFor(aPerformance) {
       const plays = require("../json/plays.json");
       return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(aPerformance) {
-      let result = Math.max(aPerformance.audience - 30, 0);
-
-      // 희극 관객 5명마다 추가 포인트를 제공한다.
-      if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-      return result;
     }
   }
 
