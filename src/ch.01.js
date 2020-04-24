c7ed6b4e6765976bd789d355cd4708f2677df714
Yun Seokjoon <seokjoon.yun@gmail.com>
2020-04-24 14:26:45 +0900

    반복문을 파이프라인으로 바꾸기

--- src/ch.01.js
+++ src/ch.01.js
@@ -77,19 +77,11 @@ function statement(invoice) {
   return renderPlainText(statementData);
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
