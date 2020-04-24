e973d65966f7b37e007d2f7ddbadd2014affe5cd
Yun Seokjoon <seokjoon.yun@gmail.com>
2020-04-24 14:31:39 +0900

    enrichPerformance 를 createStatementData 내부로 이동

--- src/ch.01.js
+++ src/ch.01.js
@@ -1,52 +1,5 @@
 "use strict";
 
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-
-  return result;
-
-  function playFor(aPerformance) {
-    const plays = require("../json/plays.json");
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르:  ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = Math.max(aPerformance.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-}
-
 function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
@@ -78,6 +31,53 @@ function statement(invoice) {
     result.totalAmount = totalAmount(result);
     result.totalVolumeCredits = totalVolumeCredits(result);
     return result;
+
+    function enrichPerformance(aPerformance) {
+      const result = Object.assign({}, aPerformance);
+      result.play = playFor(result);
+      result.amount = amountFor(result);
+      result.volumeCredits = volumeCreditsFor(result);
+
+      return result;
+
+      function playFor(aPerformance) {
+        const plays = require("../json/plays.json");
+        return plays[aPerformance.playID];
+      }
+
+      function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (aPerformance.play.type) {
+          case "tragedy": // 비극
+            result = 40000;
+            if (aPerformance.audience > 30) {
+              result += 1000 * (aPerformance.audience - 30);
+            }
+            break;
+
+          case "comedy": // 희극
+            result = 30000;
+            if (aPerformance.audience > 20) {
+              result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+
+          default:
+            throw new Error(`알 수 없는 장르:  ${aPerformance.play.type}`);
+        }
+        return result;
+      }
+
+      function volumeCreditsFor(aPerformance) {
+        let result = Math.max(aPerformance.audience - 30, 0);
+
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+      }
+    }
 
     function totalAmount(data) {
       return data.performances.reduce((total, p) => total + p.amount, 0);
